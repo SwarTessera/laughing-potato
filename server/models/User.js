e69cc91e25ae15e99cb892e0746f95a7f4ed3245
cherylcherry <cherylcherry95@gmail.com>
2016-03-02 21:43:19 +0530

    Adder user model

--- server/models/User.js
+++ server/models/User.js
@@ -5,17 +5,14 @@ var bcrypt = require('bcrypt-nodejs');
 //User Schema
 var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true },
-  password: String,
+  question: String,
+  answer: String,
 
-  facebook: String,
-  google: String,
   tokens: Array,
 
   profile: {
     name: { type: String, default: '' },
     gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
     picture: { type: String, default: '' }
   },
 
@@ -26,30 +23,30 @@ var userSchema = new mongoose.Schema({
 /**
  * Password hash middleware.
  */
-userSchema.pre('save', function(next) {
-  var user = this;
-  if (!user.isModified('password')) return next();
-  bcrypt.genSalt(10, function(err, salt) { //salt is a key for hashing value for encryption
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
-});
+// userSchema.pre('save', function(next) {
+//   var user = this;
+//   if (!user.isModified('password')) return next();
+//   bcrypt.genSalt(10, function(err, salt) { //salt is a key for hashing value for encryption
+//     if (err) return next(err);
+//     bcrypt.hash(user.password, salt, null, function(err, hash) {
+//       if (err) return next(err);
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
 
 /**
  * Helper method for validating user's password.
  */
-userSchema.methods.comparePassword = 
-	function(candidatePassword, cb) {
-  		bcrypt.compare(candidatePassword, this.password, 
-  	function(err, isMatch) {
-    	if (err) return cb(err);
-    	cb(null, isMatch);
-  });
-};
+// userSchema.methods.comparePassword = 
+// 	function(candidatePassword, cb) {
+//   		bcrypt.compare(candidatePassword, this.password, 
+//   	function(err, isMatch) {
+//     	if (err) return cb(err);
+//     	cb(null, isMatch);
+//   });
+// };
 
 // Compile Schema into a mongoose Model
 module.exports = mongoose.model('User', userSchema);
