018ba4455d667c962f8016999f945723ab56d49e
cherylcherry <cherylcherry95@gmail.com>
2016-03-10 22:10:29 +0530

    Added Icon model

---
+++ server/models/Icon.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+//Icon Schema
+var iconSchema = new mongoose.Schema({
+  name: { type: String, unique: true, lowercase: true },
+  picture: { type: String, default: '' }
+});
+
+
+// Compile Schema into a mongoose Model
+module.exports = mongoose.model('Icon', iconSchema);
