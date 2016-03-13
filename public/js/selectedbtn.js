0c89934f7ef58d44fb87270dc6161ea34d82f8f8
cherylcherry <cherylcherry95@gmail.com>
2016-03-13 21:25:10 +0530

    Only 4 images can be selected

--- public/js/selectedbtn.js
+++ public/js/selectedbtn.js
@@ -1,3 +1,30 @@
+var x=0;
 $('.icon').click(function(){
-  $(this).toggleClass('shadow');
+
+		if($(this).hasClass('shadow'))
+		{
+  			$(this).removeClass('shadow');
+  			x--;
+  			$("#msg1").text('');
+		}
+		else
+  		{
+  			if(x<4)
+  			{
+  				$(this).addClass('shadow');
+  				x++;
+  			}
+  			else
+				$("#msg1").text('bello!');
+				//alert("Hi");
+
+  				// $.jAlert({
+  				//     'title': 'It works!',
+  				//     'content': 'YAY!',
+  				//     'theme': 'green',
+  				//     'btns': { 'text': 'close' }
+  				//   });
+  				//$("#msg1").html("You have alredy selected 4 icons!");
+  		}
+	
 });
