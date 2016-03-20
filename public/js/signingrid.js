d4caa2e24975001decdc70dbe2c85df9d0d45d4c
cherylcherry <cherylcherry95@gmail.com>
2016-03-20 14:37:16 +0530

    random password components displayed

--- public/js/signingrid.js
+++ public/js/signingrid.js
@@ -1,23 +1,39 @@
 function session()
 {
-	//alert('hi');
+	//salert('hi');
 	//document.getElementById('2|2').innerHTML='hello';
 
 	var usedIcons = new Array(60);
 	var usedPwrd = new Array(4);
 	//defining 2D matix as JS doesn't support 2D matrix
 	var usedPosition = new Array(5);
-	for (i=0; i<12; i++)
+	for (var i=0; i<12; i++)
 		usedPosition[i]=new Array(12);
 
 	//select seed cell
-	var x=Math.floor(Math.random()*4);
-	var y=Math.floor(Math.random()*12);
+	//3 & 10 instead of 5 and 12 so that the grid doesn't exceed the boundary
+	var x=Math.floor(Math.random()*3);
+	var y=Math.floor(Math.random()*10);
 	usedPosition[x][y]=true;
 
 	//select seed password component
-	var p=Math.ceil(Math.random()*4);
-	usedPwrd[p]=true;
-
-	document.getElementById(x+'|'+y).innerHTML=x+'|'+y;
+	var p = new Array(4);
+	for (var i=0; i<4; i++)
+	{
+		var x=Math.ceil(Math.random()*4);
+		if (!usedPwrd[x])
+		{
+			usedPwrd[x]=true;
+			p[i]=x;
+		}
+		else
+			i--;
+	}
+	//plot the vertices
+	var x1=x+1;		var y1=y+1;
+	var x2=x1+2;	var y2=y1+2;
+	document.getElementById(x1+'|'+y1).innerHTML=p[0];
+	document.getElementById(x1+'|'+y2).innerHTML=p[1];
+	document.getElementById(x2+'|'+y1).innerHTML=p[2];
+	document.getElementById(x2+'|'+y2).innerHTML=p[3];
 }
