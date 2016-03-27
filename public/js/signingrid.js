59e120eb4f916c2c855b2586adf5d1fed3cb93ef
cherylcherry <cherylcherry95@gmail.com>
2016-03-27 10:07:01 +0530

    randomly displayed nos. for signin

--- public/js/signingrid.js
+++ public/js/signingrid.js
@@ -1,5 +1,6 @@
-var p = new Array(4);
-var pwd_set = new Array(4);
+var pwd_set = new Array(4);	//reads user's password values
+var p = new Array(4);		//randomizes password compnents
+var flag=0;					//to maintain cel position
 
 function session(a, b, c, d)
 {
@@ -10,55 +11,79 @@ function session(a, b, c, d)
 	pwd_set[2]=c;
 	pwd_set[3]=d;
 
-	var usedIcons = new Array(60);
+	var usedIcons = new Array(61);
 	var usedPwrd = new Array(4);
 	//defining 2D matix as JS doesn't support 2D matrix
-	var usedPosition = new Array(5);
-	for (var i=0; i<12; i++)
-		usedPosition[i]=new Array(12);
+	var usedPosition = new Array(6);
+	for (var i=0; i<6; i++)
+		usedPosition[i]=new Array(13);
 
 	//select seed cell
 	//3 & 10 instead of 5 and 12 so that the grid doesn't exceed the boundary
-	var x=Math.floor(Math.random()*3);
-	var y=Math.floor(Math.random()*10);
-	usedPosition[x][y]=true;
+	///Math.floor((Math.random() * max) + min);
+	var x1=Math.floor((Math.random() * 3) + 1)
+	var y1=Math.floor((Math.random() * 10) + 1)
 
 	//select random password component
-	// var p = new Array(4);
 	for (var i=0; i<4; i++)
 	{
 		var x=Math.floor(Math.random()*4);
 		if (!usedPwrd[x])
 		{
-			usedPwrd[x]=true;
-			//-var xx='i'+x;
+			usedPwrd[x]=true;		//password component taken
 			p[i]=pwd_set[x];
+			usedIcons[p[i]]=true;	//icon has been displayed
 		}
 		else
 			i--;
 	}
 
 	//plot the vertices
-	var x1=x+1;		var y1=y+1;
-	var x2=x1+2;	var y2=y1+2;
+	var x2=x1+2;		var y2=y1+2;
+	
 	document.getElementById(x1+'|'+y1).innerHTML=p[0];
+	usedPosition[x1][y1]=true;
 	document.getElementById(x1+'|'+y2).innerHTML=p[1];
+	usedPosition[x1][y2]=true;
 	document.getElementById(x2+'|'+y1).innerHTML=p[2];
+	usedPosition[x2][y1]=true;
 	document.getElementById(x2+'|'+y2).innerHTML=p[3];
+	usedPosition[x2][y2]=true;
 
-	// //display remaining images randomly
-	// for (var i = 0; i < 5; i++)
-	// { 
-	// 	for (var j = 0; j < 12; j++) 
-	// 	{
-	// 	//low (inclusive) and high (inclusive) 
-	// 	//Math.floor(Math.random() * (high - low+1) + low);
-	// 	var decision=Math.floor(Math.random()*25);
-	// 	if (!usedLines[decision])
-	// 	{
-	// 		usedLines[decision]=true;
-	// 		document.getElementById('line'+i).innerHTML=paragraphArray[decision];
-	// 	}
-	// 	}	
-	// }
+	alert(x1+'  '+y1+'|'+x1+'  '+y2+'|'+x2+'  '+y1+'|'+x2+'  '+y2);
+
+	//display remaining images randomly
+	for (var i = 1; i <= 5; i++)
+	//var i = 1;
+	//while(i <= 5)
+	{ 
+		//alert(i);
+		for (var j = 1; j <= 12; j++) 
+		//var j = 1;
+		//while(j <= 12)
+		{
+			//alert(i+'  '+j);
+			if (!usedPosition[i][j])
+			{
+				//alert('position');
+				var x=Math.floor((Math.random() * 60) + 1)
+				if (!usedIcons[x])
+				{
+					usedIcons[x]=true;
+					usedPosition[i][j]=true;
+					document.getElementById(i+'|'+j).innerHTML=x;
+					alert('set '+i+','+j);
+					flag=0;
+					//j++;
+				}
+				else //if (flag == 0)
+				{
+					alert(x+' is used');
+					j--;
+					flag=1;
+				}
+			}
+		}
+		//i++;
+	}
 }
