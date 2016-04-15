722c388bd87e060a119f94970ea911ef2b4d68af
cherylcherry <cherylcherry95@gmail.com>
2016-04-15 11:47:04 +0530

    Revived email sending

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/signin', userController.getSignIn);
 app.post('/signin', userController.postSignIn);
 
 
-app.get("/nodemailer",emailer.getMailer);
+app.post("/nodemailer",emailer.postMailer);
 
 app.listen(3020);
 console.log("Express is listening at port 3020");
--- server/config/email.js
+++ server/config/email.js
@@ -1,7 +1,7 @@
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
 
-exports.getMailer=function(req,res){
+exports.postMailer=function(req,res){
 
    //your nodemailer logic here to send mail
 
@@ -13,24 +13,26 @@ exports.getMailer=function(req,res){
       }
    });
 
-   // res.render('demo', {
-   //   name: 'Satellizer L. Bridget'
-   // }, function(err, html) {
-
     var mailOptions = {
       from: "SwarTessera <swartessera@gmail.com>", // sender address
       to: "Test <test.swartessera@gmail.com>",//, Venz <venzfalcao@gmail.com>, Chi <ajobard96@gmail.com>", // comma separated list of receivers
       subject: "Test mail with html attached from SwarTessera", // Subject line
-      //text: "Hello world  - this test e-mail is sent from SwarTessera. o.O", // plaintext body
+      text: "Hello world  - this test e-mail is sent from SwarTessera. o.O"//, // plaintext body
       //<b>Hello world ✔</b><br><i>This text is actually HTML.</i>
       //'Embedded image: <img src="http://i.imgur.com/e13TSsJ.png">;'
       //forceEmbeddedImages: true,
-      html: '<b>Hello world ✔</b>' // You can choose to send an HTML body 
+      // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body 
     };
 
-     smtpTransport.sendMail(mailOptions, function(err){
-          console.log("Mail sent! ");
-     });
-   // });
+     smtpTransport.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.json({yo: 'error'});
+        }
+        else{
+            console.log('Message sent: ' + info.response);
+            res.json({yo: info.response});
+        };
+    });
 }
 
